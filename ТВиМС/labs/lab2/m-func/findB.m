f993b2ec417044e44b4e0336860005cb5e238120
xsnakee <xsnakee@gmail.com>
2018-03-13 23:41:38 +0300

    -update TViMS

---
+++ ТВиМС/labs/lab2/m-func/findB.m
@@ -0,0 +1,29 @@
+m = 5;
+n = 10000;
+
+A = rand(m,n);
+
+%вариант 4
+C = [0.3 0.8;0.3 0.8;0.3 0.8; 0.20 0.25; 0.06 0.096];
+
+for i = 1:m
+    for j = 1:n
+        B(i,j) = logzn(C(i,1),C(i,2),A(i,j));
+    end
+end
+
+for i = 1:m
+    for j = 1:n
+        y(i,j) = fregp(B(i,:),j);
+    end
+end
+
+subplot(2,1,1);
+plot(y(1,:));
+xlabel('Количество событий');
+ylabel('Частота события Z');
+
+subplot(2,1,2);
+semilogx(y(1,:));
+xlabel('Количество событий');
+ylabel('Частота события Z');
---
+++ ТВиМС/labs/lab2/m-func/logzn.m
@@ -0,0 +1,7 @@
+function y = logzn(am,aM,x)
+%bla bla bla
+if ((am <= x) && (aM>=x)) 
+    y = 1;
+else
+    y = 0;
+end
