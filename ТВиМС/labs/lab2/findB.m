bc88c88cd5b9cf3bc78722ccdd1539797d0a1258
xsnakee <xsnakee@gmail.com>
2018-03-18 22:14:19 +0300

    -updated TVims lab_2 -updated electro lab_2

--- ТВиМС/labs/lab2/findB.m
+++ ТВиМС/labs/lab2/findB.m
@@ -4,14 +4,34 @@ n = 1000;
 A = rand(m,n);
 
 %вариант 4
-C = [0.3 0.8;0.3 0.8;0.3 0.8; 0.20 0.25; 0.06 0.096];
+C = [0.3 0.8;
+    0.3 0.8;
+    0.3 0.8; 
+    0.20 0.25; 
+    0.06 0.96];
 
-for i = 1:size(A,1)
-    for j = 1:size(A,2)
-        if (logzn(C(i,1),C(i,2),A(i,j)))
-            B(i,j) = 1;
-        else
-            B(i,j) = 0;
-        end
+for i = 1:m
+    for j = 1:n
+        B(i,j) = logzn(C(i,1),C(i,2),A(i,j));
     end
+end
+
+for i = 1:m
+    for j = 1:n
+        y(i,j) = fregp(B(i,:),j);
+    end
+end
+
+for i = 1:m
+%subplot(2,1,1);
+plot(y(i,:));
+xlabel('Количество событий');
+ylabel('Частота события Z');
+
+pause;
+%subplot(2,1,2);
+semilogx(y(i,:));
+xlabel('Количество событий');
+ylabel('Частота события Z');
+pause;
 end
